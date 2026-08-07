ffc463c057f057ee7ac80aca7077528264095554
sseraflm <anomyuslol@gmail.com>
2026-08-07 14:47:14 +0200

    feat: add input support and operations history

--- calculator.js
+++ calculator.js
@@ -18,28 +18,40 @@ function divide(a, b) {
     return a / b;
 }
 
-const a = Number(prompt("Type the first number:", ""));
-const b = Number(prompt("Type the second number:", ""));
-const operator = prompt("Type the operator you wish to use:", "");
-
-function calculate(a, operator, b) {
-    switch (operator) {
-        case "+":
-            return add(a, b);
-
-        case "-":
-            return subtract(a, b);
-
-        case "*":
-            return multiply(a, b);
-
-        case "/":
-            return divide(a, b);
-
-        default:
-            return "Invalid operator";
+let numberOne = document.getElementById("numberOne");
+let numberTwo = document.getElementById("numberTwo");
+let operator = document.getElementById("operator");
+let calculateButton = document.getElementById("calculate");
+let history = document.getElementById("history");
+function calculate() {
+    if (numberOne.value === "" || numberTwo.value === "") {
+        return;
     }
+    let a = Number(numberOne.value);
+    let b = Number(numberTwo.value);
+    let result;
+    switch (operator.value) {
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        default:
+            result = "Invalid operator";
+    }
+    let newP = document.createElement("p");
+    newP.innerText = `${a} ${operator.value} ${b} = ${result}`;
+    newP.classList.add("historyPar");
+    history.append(newP);
+    numberOne.value = "";
+    numberTwo.value = "";
 }
 
-const result = calculate(a, operator, b);
-console.log(result);
+calculateButton.addEventListener("click", calculate);
