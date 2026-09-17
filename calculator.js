a0065ef9056d97650a7c30a4428bd428f685bb5d
sseraflm <anomyuslol@gmail.com>
2026-09-18 00:26:34 +0200

    refactor: Replace let into const across the script

--- calculator.js
+++ calculator.js
@@ -1,57 +1,57 @@
 "use strict";
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 function subtract(a, b) {
-    return a - b;
+  return a - b;
 }
 
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        return "You cannot divide by zero.";
-    }
-    return a / b;
+  if (b === 0) {
+    return "You cannot divide by zero.";
+  }
+  return a / b;
 }
 
-let numberOne = document.getElementById("numberOne");
-let numberTwo = document.getElementById("numberTwo");
-let operator = document.getElementById("operator");
-let calculateButton = document.getElementById("calculate");
-let history = document.getElementById("history");
+const numberOne = document.getElementById("numberOne");
+const numberTwo = document.getElementById("numberTwo");
+const operator = document.getElementById("operator");
+const calculateButton = document.getElementById("calculate");
+const history = document.getElementById("history");
 function calculate() {
-    if (numberOne.value === "" || numberTwo.value === "") {
-        return;
-    }
-    let a = Number(numberOne.value);
-    let b = Number(numberTwo.value);
-    let result;
-    switch (operator.value) {
-        case "+":
-            result = add(a, b);
-            break;
-        case "-":
-            result = subtract(a, b);
-            break;
-        case "*":
-            result = multiply(a, b);
-            break;
-        case "/":
-            result = divide(a, b);
-            break;
-        default:
-            result = "Invalid operator";
-    }
-    let newP = document.createElement("p");
-    newP.innerText = `${a} ${operator.value} ${b} = ${result}`;
-    newP.classList.add("historyPar");
-    history.append(newP);
-    numberOne.value = "";
-    numberTwo.value = "";
+  if (numberOne.value === "" || numberTwo.value === "") {
+    return;
+  }
+  const a = Number(numberOne.value);
+  const b = Number(numberTwo.value);
+  let result;
+  switch (operator.value) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      result = divide(a, b);
+      break;
+    default:
+      result = "Invalid operator";
+  }
+  const newP = document.createElement("p");
+  newP.innerText = `${a} ${operator.value} ${b} = ${result}`;
+  newP.classList.add("historyPar");
+  history.append(newP);
+  numberOne.value = "";
+  numberTwo.value = "";
 }
 
 calculateButton.addEventListener("click", calculate);
